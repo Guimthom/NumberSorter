8f7e4173e775dba800e4574e15d81dd36a998f0a
Pixel Gui <guillaumethomart@gmail.com>
2024-05-23 11:41:25 +0200

    selection sort function

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const sortInputArray = (event) => {
     ...document.getElementsByClassName("values-dropdown")
   ].map((dropdown) => Number(dropdown.value));
 
-  const sortedValues = bubbleSort(inputValues);
+  const sortedValues = selectionSort(inputValues);
 
   updateUI(sortedValues);
 }
@@ -20,18 +20,22 @@ const updateUI = (array = []) => {
 }
 
 // Triage bubble (on déplace un nombre jusqu'a ce que l'expression se valide, puis on passe à un autre)
-const bubbleSort = (array) => {
-  for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array.length - 1; j++) {
-      if (array[j] > array[j + 1]) {
-        const temp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = temp;
+const selectionSort = (array) => {
+    for (let i = 0; i < array.length; i++) {
+      let minIndex = i;
+  
+      for (let j = i + 1; j < array.length; j++) {
+        if (array[j] < array[minIndex]) {
+          minIndex = j;
+        }
       }
+  
+      const temp = array[i];
+      array[i] = array[minIndex];
+      array[minIndex] = temp;
     }
+  
+    return array;
   }
-
-  return array;
-}
 
 sortButton.addEventListener("click", sortInputArray);
